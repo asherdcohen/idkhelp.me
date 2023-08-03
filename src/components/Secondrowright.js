3cee7e11b23433893120ca1a6b4ad783984fef48
Asher Cohen <asherdcohen@gmail.com>
2023-08-03 16:48:03 -0400

    Changed photo to gif

--- src/components/Secondrowright.js
+++ src/components/Secondrowright.js
@@ -1,10 +1,24 @@
+// import React from "react";
+// import backgroundImage from "../images/Simplify.png";
+
+// const Secondrowright = () => {
+//     return (
+//         <div className="second-row-right">
+//             <img src={backgroundImage} alt="Simplify" className="simplify-image" />
+//         </div>
+//     );
+// };
+
+// export default Secondrowright;
+
 import React from "react";
-import backgroundImage from "../images/Simplify.png";
+import gifBackground from "../images/gif.gif";
+import "./Secondrowright.css"; // You may need to adjust the path based on your project structure
 
 const Secondrowright = () => {
     return (
         <div className="second-row-right">
-            <img src={backgroundImage} alt="Simplify" className="simplify-image" />
+            <img src={gifBackground} alt="Simplify GIF" className="simplify-gif" />
         </div>
     );
 };
